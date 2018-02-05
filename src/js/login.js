bb9d9d4bb2fc015743ab50a4f4a04341f1c90bd0
Lee_PeiPeiYun <191440042@qq.com>
2018-02-05 11:58:04 +0800

    ie 能用

--- src/js/login.js
+++ src/js/login.js
@@ -1,15 +1,15 @@
 !function () {
   getAjax('http://www.yhcqp.com/system/members', "", function (res) {
     if(!res.data) return
-      let companysDOM = document.getElementById('companys')
-      let companys = res.data
-      let companysLength = companys.length
-      for(let i = 0, j = (parseInt(companys.length / 6) + 1) * 6; i < j; i ++) {
-        let isEmpty = i >= companysLength
-        let company = isEmpty ? { name: '敬请期待' } : companys[i]
+      var companysDOM = document.getElementById('companys');
+      var companys = res.data;
+      var companysLength = companys.length;
+      for(var i = 0, j = (parseInt(companys.length / 6) + 1) * 6; i < j; i ++) {
+        var isEmpty = i >= companysLength;
+        var company = isEmpty ? { name: '敬请期待' } : companys[i]
 
-        let li = ""
-        let className = ""
+        var li = ""
+        var className = ""
         if(parseInt(i / 6) % 2 === 1) {
           className = "even-row"
           if(isEmpty) className += ' empty'
@@ -23,18 +23,18 @@
       }
   })
 }()
+var local = ["ppy-alliance-username","ppy-alliance-password"];
 
-var local = ["ppy-alliance-username","ppy-alliance-password"]
-var _username = localStorage.getItem(local[0]) || $('#username').val()
-var  _password = localStorage.getItem(local[1]) || $('#password').val() 
-  $('#username').val(_username)
-  $('#password').val(_password)
+var _username = $.cookie(local[0]) || $('#username').val();
+var  _password = $.cookie(local[1]) || $('#password').val(); 
+  $('#username').val(_username);
+  $('#password').val(_password);
 $('#username').bind('input propertychange', function() {  
-    _username = $('#username').val() 
+    _username = $('#username').val(); 
 });
 
 $('#password').bind('input propertychange', function() {  
-    _password = $('#password').val()
+    _password = $('#password').val();
 });
 
  /*===============*/
@@ -42,11 +42,11 @@ $('#password').bind('input propertychange', function() {
  $("#login-btn").on('click', function(event) {
       event.preventDefault();
       if(_username.length < 1) {
-          alert('请输入用户名')
+          alert('请输入用户名');
           return
       }
       if(_password.length < 1) {
-          alert('请输入密码')
+          alert('请输入密码');
           return
       }
       var _url = "http://www.yhcqp.com/community/login"
@@ -57,8 +57,8 @@ $('#password').bind('input propertychange', function() {
 
       postAjax(_url, _obj, function(res) {
         if ($('#remeber-password').attr('checked')) {
-            localStorage.setItem(local[0],_username)
-            localStorage.setItem(local[1],_password)
+            $.cookie(local[0],_username);
+            $.cookie(local[1],_password);
         }
           // location.href = "/"
       })
@@ -66,39 +66,39 @@ $('#password').bind('input propertychange', function() {
  // regist
  $("#regist-btn").on('click', function(event) {
    event.preventDefault();
-   location.href='/regist.html'
+   location.href='/regist.html';
  });
  // forget
  $("#forget-btn").on('click',  function(event) {
    event.preventDefault();
-   $("#login").css("display","none")
-   $("#forget").css("display","flex")
+   $("#login").css("display","none");
+   $("#forget").css("display","flex");
  });
 
  $("#back-btn").on('click', function(event) {
    event.preventDefault();
-   $("#login").css("display","flex")
-   $("#forget").css("display","none")
+   $("#login").css("display","flex");
+   $("#forget").css("display","none");
  });
 
  // check
- var _phone = localStorage.getItem('ppy-alliance-username') || $.trim($("#phone-reset").val())
  $("#check-btn").on('click', function(event) {
+  var _phone = $.cookie('ppy-alliance-username') || $.trim($("#phone-reset").val())
     event.preventDefault();
-    var isLoop = false
-    var loopTime = 60
+    var isLoop = false;
+    var loopTime = 60;
     
     if(this.isLoop || _phone.length < 11) return
     else this.isLoop = true
 
     var _passobj = { 
-      action: 'findpwd',      
+      action: 'findpwd'      
     } 
-     _passobj.phone = _phone
+     _passobj.phone = _phone;
     getAjax('/community/base/sms_code', _passobj, function(res) {
           var checkLoop = setInterval(function() {
-            var _loopTime = --loopTime + 's'
-            $("#check-btn").text(_loopTime)
+            var _loopTime = --loopTime + 's';
+            $("#check-btn").text(_loopTime);
             if(loopTime < 1) {
               loopTime = 60
               $("#check-btn").text("获取验证码")
@@ -113,11 +113,12 @@ $('#password').bind('input propertychange', function() {
   $("#submit-btn").on('click', function(event) {
       event.preventDefault();
       // _phone
+      var _phones = $.cookie('ppy-alliance-username') || $.trim($("#phone-reset").val())
       var _sms_code = $.trim($("#code-reset").val()) 
       var _password = $.trim($("#pass-reset").val())
       var _checkPassword = $.trim($("#checkpass-reset").val())
       
-      if(_phone.length < 11) {
+      if(_phones.length < 11) {
         alert('请输入手机号')
         return
       }else if(_sms_code.length < 1) {
@@ -132,7 +133,7 @@ $('#password').bind('input propertychange', function() {
       }
 
       var _resetobj = {
-          phone: _phone,
+          phone: _phones,
           sms_code:_sms_code,
           password: _password
       }
--- src/js/regist.js
+++ src/js/regist.js
@@ -2,6 +2,107 @@
 * @Author: steven
 * @Date:   2018-02-01 18:30:03
 * @Last Modified by:   steven
-* @Last Modified time: 2018-02-01 18:30:07
+* @Last Modified time: 2018-02-02 13:41:59
 */
 
+function selectCity (){
+	
+}
+
+// 创建生成类型列表
+function buildTypes (){
+  	var companys = ['汽配商', '修理厂', '4S店', '其他'];
+  	var companysLength = companys.length
+      for(var i = 0, j = companysLength; i < j; i ++) {
+        var classname  = "type" 
+        var btn = "<button class=\"" + classname + "\">" + companys[i] + "</button>"
+        $("#reg-types").prepend(btn) 
+      }
+     $(".type").on('click', function() {
+		$("#reg-types button").attr("class","type")
+		$(this).attr("class","type-selected")
+	});
+}
+
+
+// sms_code
+ $("#check-btn").on('click', function(event) {
+ 	var _phone = $.trim($("#reg-phone").val())
+    var isLoop = false
+    var loopTime = 60
+
+    if(this.isLoop || _phone.length < 11) return
+    else this.isLoop = true
+    var _passobj = { 
+      action: 'register',      
+    } 
+     _passobj.phone = _phone
+    getAjax('/community/base/sms_code', _passobj, function(res) {
+          var checkLoop = setInterval(function() {
+            var _loopTime = --loopTime + 's'
+
+            $("#check-btn").text(_loopTime)
+            if(loopTime < 1) {
+              loopTime = 60
+              $("#check-btn").text("获取验证码")
+              isLoop = false
+              clearInterval(checkLoop)
+            }
+          }, 1000)
+      })
+ });
+
+$("#reg-checked").on('click', function () {
+  $(".delegate-container").css("display","block");
+})
+
+$(".d-close").on("click", function () {
+  $(".delegate-container").css("display","none");
+})
+
+$(".d-check").on("click", function () {
+  $(".delegate-container").css("display","none");
+  $("#reg-checked").attr("checked",true);
+})
+
+//注册按钮确定
+$("#reg-checked").on('click', function (event) {
+      event.preventDefault();
+      // _phone
+      var _phones = $.trim($("#reg-phone").val())
+      var _sms_code = $.trim($("#reg-sms_code").val()) 
+      var _password = $.trim($("#reg-password").val())
+      var _checkPassword = $.trim($("#checkpass-reset").val())
+      
+      if(_phones.length < 11) {
+        alert('请输入手机号')
+        return
+      }else if(_sms_code.length < 1) {
+        alert('请输入验证码')
+        return
+      }else if(_password.length < 1) {
+        alert('请输入密码')
+        return
+      }else if(_checkPassword !== _password) {
+        alert('请输入相同的确认密码')
+        return
+      }
+
+      var _resetobj = {
+          phone: _phones,
+          sms_code:_sms_code,
+          password: _password
+      }
+
+      postAjax('/community/user/find_pwd', _resetobj, function(res) {
+        alert('修改密码成功')
+        $("#login").css("display","flex")
+        $("#forget").css("display","none")
+      })
+})
+
+//初始化页面
+!function initHtml(){
+	buildTypes()
+	$(".type").first().attr("class","type-selected")
+}()
